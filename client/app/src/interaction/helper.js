49129c3c937256f1bf2bde89dc1e8b06b53fefac
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-05-01 23:47:32 -0400

    Added todos for a bug

--- client/app/src/interaction/helper.js
+++ client/app/src/interaction/helper.js
@@ -1,6 +1,8 @@
 import { RESIZE_AREA_CORNER, RESIZE_AREA_MIN } from "./constants";
 
 // TODO: Resizing is still a little jumpy
+// TODO: Resizing is still a little jumpy
+// TODO: Resizing is still a little jumpy
 
 export const makeSpriteInteractive = (sprite) => {
     sprite.interactive = true;  // Allow sprite to interact with pointer events
