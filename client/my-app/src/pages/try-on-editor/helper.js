a36c8d44221ce67fcff0b9a20fa205759c24c4fa
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-09-15 23:24:17 -0400

    Prettifying files

--- client/my-app/src/pages/try-on-editor/helper.js
+++ client/my-app/src/pages/try-on-editor/helper.js
@@ -1,16 +1,16 @@
-import { Texture, Sprite } from 'pixi.js';
+import { Texture, Sprite } from "pixi.js";
 
 export const createSpriteFromFile = (file, callback) => {
-    const reader = new FileReader();
-    reader.onload = (e) => {
-        const img = new Image();
-        img.src = e.target.result;
-        img.onload = () => {
-            const texture = Texture.from(img);
-            const sprite = new Sprite(texture);
-            sprite.anchor.set(0.5);
-            callback(sprite);
-        };
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const img = new Image();
+    img.src = e.target.result;
+    img.onload = () => {
+      const texture = Texture.from(img);
+      const sprite = new Sprite(texture);
+      sprite.anchor.set(0.5);
+      callback(sprite);
     };
-    reader.readAsDataURL(file);
-}
+  };
+  reader.readAsDataURL(file);
+};
--- client/my-app/src/pixi/contexts/PixiContext.jsx
+++ client/my-app/src/pixi/contexts/PixiContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useRef } from 'react';
+import React, { createContext, useContext, useRef } from "react";
 
 // TODO: Convert this to typescript so that definitions can show up in IDE
 
@@ -7,19 +7,21 @@ const PixiContext = createContext();
 export const usePixi = () => useContext(PixiContext);
 
 export const PixiProvider = ({ children }) => {
-    const appRef = useRef(null);
-    const charContainerRef = useRef(null);
-    const wardrobeContainerRef = useRef(null);
-    const itemsContainerRef = useRef(null);
+  const appRef = useRef(null);
+  const charContainerRef = useRef(null);
+  const wardrobeContainerRef = useRef(null);
+  const itemsContainerRef = useRef(null);
 
-    return (
-        <PixiContext.Provider value={{
-            appRef,
-            charContainerRef,
-            wardrobeContainerRef,
-            itemsContainerRef,
-        }}>
-            {children}
-        </PixiContext.Provider>
-    );
+  return (
+    <PixiContext.Provider
+      value={{
+        appRef,
+        charContainerRef,
+        wardrobeContainerRef,
+        itemsContainerRef,
+      }}
+    >
+      {children}
+    </PixiContext.Provider>
+  );
 };
