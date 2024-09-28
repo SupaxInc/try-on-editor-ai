71831ee428b572e411d45e05a407e54573a8912e
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-09-28 17:48:08 -0400

    Updating pixi helper file to typescript and adding new method to grab application from sprite

---
+++ client/my-app/src/pixi/utils/helper.ts
@@ -0,0 +1,51 @@
+import { Container, Graphics, Sprite, Texture, Application } from "pixi.js";
+import { AllSetupSprites } from "../types";
+import { MutableRefObject } from "react";
+
+export const drawDebugBounds = (container: Container): void => {
+  const graphics = new Graphics();
+  graphics.stroke({ width: 2, color: 0xff0000, alpha: 1 });
+  graphics.rect(0, 0, container.width, container.height);
+  container.addChild(graphics);
+  console.log("Container dimensions:", container.width, container.height);
+};
+
+// TODO: Possibly add responsive scaling?
+export const scaleSpriteToFitContainer = (
+  sprite: Sprite,
+  containerRef: MutableRefObject<Container>,
+  padding: number = 0
+): void => {
+  // Add padding to the container width and height, multiply by 2 to account for top/bottom, left/right sides.
+  // Adds padding to container to effectively make container smaller or bigger for better scaling
+  const effectiveWidth = containerRef.current.width - padding * 2;
+  const effectiveHeight = containerRef.current.height - padding * 2;
+
+  // Texture represents original dimensions of the sprite
+  // Sprite dimensions (sprite.height, .width) are affected by the sprite's scale properties
+  const texture: Texture = sprite.texture;
+  const spriteWidth = texture.width;
+  const spriteHeight = texture.height;
+
+  const scaleX = effectiveWidth / spriteWidth;
+  const scaleY = effectiveHeight / spriteHeight;
+
+  // Use the smallest scale factor to maintain aspect ratio
+  const scale = Math.min(scaleX, scaleY);
+
+  // Scale the sprite's scale x and y to the scale factor
+  sprite.scale.set(scale);
+};
+
+export const getApplicationFromSprite = (
+  sprite: AllSetupSprites
+): Application | null => {
+  let current: Container | null = sprite;
+  while (current) {
+    if ("stage" in current) {
+      return (current as any).stage.application as Application;
+    }
+    current = current.parent;
+  }
+  return null;
+};
