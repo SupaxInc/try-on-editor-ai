9ee6665dbc2f90df25c42fcf922fe75dfb2f1214
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-09-30 00:02:13 -0400

    Changing how its finding the characterSprite

--- client/my-app/src/components/wardrobe/Wardrobe.tsx
+++ client/my-app/src/components/wardrobe/Wardrobe.tsx
@@ -116,11 +116,13 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
     }
 
     const newItemSprite = itemSprites[itemSprites.length - 1] as ItemSprite;
+    // TODO: This may need to be better handled, as it's hardcoding the character sprite container
     const characterSprite = fittingRoomContainerRef.current?.children.find(
-      (child) => (child as Sprite).label === "characterSprite"
-    ) as CharacterSprite;
+      (child) => child.label === "characterSpriteContainer"
+    )?.children[0] as CharacterSprite;
 
     if (!characterSprite || !appRef.current) {
+      console.warn("No character sprite or app ref");
       return;
     }
 
