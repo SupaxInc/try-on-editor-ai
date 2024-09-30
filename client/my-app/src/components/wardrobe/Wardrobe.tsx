a2f8777ec4836475245521086e930e7c878c9786
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-09-29 21:18:07 -0400

    Adding comments to Wardrobe

--- client/my-app/src/components/wardrobe/Wardrobe.tsx
+++ client/my-app/src/components/wardrobe/Wardrobe.tsx
@@ -29,12 +29,13 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
 
   /* Setting up the Wardrobe and Items Containers */
   useEffect(() => {
+    // Containers scales its resolution based on its children, so use graphics to create boundaries for wardrobe
     const setWardrobeBounds = (wardrobeContainer: NamedContainer) => {
       if (!appRef.current) return;
 
       const boundary = new Graphics();
-      const wardrobeWidth = appRef.current.screen.width;
-      const wardrobeHeight = appRef.current.screen.height * 0.25;
+      const wardrobeWidth = appRef.current.screen.width; // Full width of app canvas
+      const wardrobeHeight = appRef.current.screen.height * 0.25; // Remaining 25% of the height at the bottom
 
       boundary.rect(0, 0, wardrobeWidth, wardrobeHeight);
       boundary.fill({ color: 0x333333 });
@@ -49,6 +50,7 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
       return wardrobeContainer;
     };
 
+    // Containers scales its resolution based on its children, so use graphics to create boundaries for items
     const setItemsBounds = (itemsContainer: NamedContainer) => {
       if (!wardrobeContainerRef.current) return;
 
@@ -73,26 +75,32 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
       return;
     }
 
+    /* Setup Wardrobe container */
     if (!wardrobeContainerRef.current) {
       const wardrobeContainer = setupWardrobeContainer();
       appRef.current.stage.addChild(wardrobeContainer);
       wardrobeContainerRef.current = wardrobeContainer;
+      // (0,0) origin is top left of canvas, imagine the wardrobe rectangle where origin is top left (0,0)
+      wardrobeContainerRef.current.x = 0; // Position x to start at left of canvas
 
-      wardrobeContainerRef.current.x = 0;
+      // Position y to start at wardrobe height - the app canvas's screen height, so it starts 80% of screen
       wardrobeContainerRef.current.y =
         appRef.current.screen.height - wardrobeContainerRef.current.height;
     }
 
-    if (!itemsContainerRef.current && wardrobeContainerRef.current) {
+    /* Setup Items Container */
+    if (!itemsContainerRef.current) {
       const itemsContainer = setupItemsContainer();
       wardrobeContainerRef.current.addChild(itemsContainer);
       itemsContainerRef.current = itemsContainer;
 
+      // Position at top left of wardrobe container
       itemsContainerRef.current.x = 0;
       itemsContainerRef.current.y = 0;
     }
 
     return () => {
+      // No need to destroy items container or item sprites as its children of wardrobe container
       if (wardrobeContainerRef.current) {
         wardrobeContainerRef.current.destroy({ children: true });
         wardrobeContainerRef.current = null;
@@ -116,16 +124,18 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
       return;
     }
 
+    // Scale the sprite to container before positioning calculations
     scaleSpriteToFitContainer(newItemSprite, itemsContainerRef, 15);
 
     const itemSpacing = 200;
     const newPositionX =
       itemSprites.length > 1 ? totalItemsWidthRef.current + itemSpacing : 150;
     newItemSprite.x = newPositionX;
-    newItemSprite.y = itemsContainerRef.current.height / 2;
+    newItemSprite.y = itemsContainerRef.current.height / 2; // Middle of the items container
 
+    // Storing new initialX and initialY properties to sprite object to be used for onDropResetToInitial
     newItemSprite.initialX = newPositionX;
-    newItemSprite.initialY = itemsContainerRef.current.height / 2;
+    newItemSprite.initialY = itemsContainerRef.current.height / 2; // Middle of the items container
 
     const newInteractiveItem = new InteractiveSprite(
       newItemSprite,
@@ -164,6 +174,7 @@ const Wardrobe: React.FC<{ itemSprites: Sprite[] }> = ({ itemSprites }) => {
 
     itemsContainerRef.current.addChild(newInteractiveItem.getSprite());
 
+    // Increase total items width to account for new sprites
     totalItemsWidthRef.current += newPositionX;
   }, [itemSprites, itemsContainerRef, charContainerRef, appRef]);
 
