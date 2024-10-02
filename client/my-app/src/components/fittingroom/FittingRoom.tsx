56e410a8def0ae2649837071aff7b37e6c76b5bd
Paul John Gonzales <paulj_gonzales@hotmail.com>
2024-10-01 23:49:27 -0400

    Refactoring FittingRoom to correct variable names

--- client/my-app/src/components/fittingroom/FittingRoom.tsx
+++ client/my-app/src/components/fittingroom/FittingRoom.tsx
@@ -2,18 +2,19 @@ import React, { useEffect, FC } from "react";
 import { Container, Graphics, Sprite } from "pixi.js";
 import { usePixi } from "../../pixi/contexts/PixiContext";
 import Character from "../character/Character";
+import { NamedContainer } from "../../pixi/types";
 
 const FittingRoom: FC<{ charSprite: Sprite }> = ({ charSprite }) => {
   const { appRef, fittingRoomContainerRef } = usePixi();
 
   useEffect(() => {
-    const setFittingRoomBounds = (fittingRoomContainer: Container) => {
+    const setFittingRoomBounds = (fittingRoomContainer: NamedContainer) => {
       if (!appRef.current) return;
 
       const boundary = new Graphics();
-      const charWidth = appRef.current.screen.width;
-      const charHeight = appRef.current.screen.height * 0.75; // Remaining 80% of the height at the bottom
-      boundary.rect(0, 0, charWidth, charHeight);
+      const fittingRoomWidth = appRef.current.screen.width;
+      const fittingRoomHeight = appRef.current.screen.height * 0.75; // Remaining 80% of the height at the bottom
+      boundary.rect(0, 0, fittingRoomWidth, fittingRoomHeight);
       boundary.fill({ color: 0xffffff, alpha: 1 });
       boundary.stroke({ width: 2, color: 0xff0000, alpha: 1 });
 
@@ -37,6 +38,7 @@ const FittingRoom: FC<{ charSprite: Sprite }> = ({ charSprite }) => {
         "fittingRoomContainer"
       );
       if (fittingRoomContainerRef.current) {
+        // (0,0) origin is top left of canvas, imagine the fitting rectangle where origin is top left (0,0)
         // Position x to left of canvas (parent)
         fittingRoomContainerRef.current.x = 0;
         // Position y to start at the top of the canvas
